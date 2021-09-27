a1d266ccb693f5731c1796e48fd43e51050537e0
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-09-27 11:57:40 +0300

    Initial Commit

---
+++ AccessibilityPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityPlayground
+//
+//  Created by Igor Chernyshov on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
